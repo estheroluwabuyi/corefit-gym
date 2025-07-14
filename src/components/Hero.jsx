2661048d431a9e3b86c4519c2902445a9951ae6b
Esther Okiki <estherokiki15@gmail.com>
2025-07-14 08:43:18 +0100

    feat: improve Hero section layout, text styles, and image presentation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,25 +2,25 @@ import heroImg from "/images/bg.webp";
 
 function Hero() {
   return (
-    <header className="bg-black min-h-screen pt-[15rem] hero-bg px-8 flex justify-center items-center lg:items-start flex-col lg:flex-row gap-8 ">
-      <div className="text-center lg:text-left ">
-        <h1 className="font-montserrat font-[800] text-[2.5rem] sm:text-[3.5rem] md:text-[4rem] lg:text-[7rem] uppercase">
+    <header className="bg-black pt-[15rem] lg:pt-[11rem] hero-bg px-8 flex justify-center items-center lg:items-start lg:px-[5rem] flex-col lg:flex-row gap-8 relative">
+      <div className="text-center lg:text-left self-center">
+        <h1 className="font-montserrat font-[800] text-[3rem] sm:text-[4rem] md:text-[5rem] lg:text-[6.5rem] uppercase">
           <span className="text-secondary text-shadow-orange">Conquer.</span>{" "}
           <span className="text-text text-shadow-orange">Build.</span> <br />
           <span className="text-text text-shadow-orange">Dominate.</span>{" "}
           <span className="text-secondary text-shadow-orange">Repeat.</span>
         </h1>
 
-        <p className="md:text-[1.5rem] lg:text-[1.55rem] font-[500] mt-4 md:max-w-3xl tracking-wider">
+        <p className="text-[1.2rem] md:text-[1.4rem] lg:text-[1.55rem] font-[500] mt-4 md:max-w-3xl tracking-wider">
           Stop settling. Push past limits, break through excuses, and forge
           unshakable strength. Your transformation starts here.
         </p>
-        <button className="bg-secondary py-4 px-8  lg:px-12 rounded-tl-[1.8rem] rounded-br-[1.8rem] text-[1.5rem] lg:text-[1.8rem] border-2 border-transparent font-[600] uppercase tracking-wide text-text hover:bg-transparent transition-all duration-600 hover:border-secondary mt-8">
+        <button className="bg-secondary py-4 px-8  lg:px-12 rounded-tl-[1.8rem] rounded-br-[1.8rem] text-[1.5rem] lg:text-[1.8rem] border-2 border-transparent font-[600] uppercase tracking-wide text-text hover:bg-transparent transition-all duration-600 hover:border-secondary mt-8 mb-4 lg:mb-0">
           Start Training
         </button>
       </div>
 
-      <div>
+      <div className="lg:self-end">
         <img
           src={heroImg}
           alt="Hero Image"
