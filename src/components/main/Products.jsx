187fe73025637419e695f53bec01af4af0f81163
Esther Okiki <estherokiki15@gmail.com>
2025-08-02 23:03:57 +0100

    enhance:add text-secondary to cart img when hovered on mobile

--- src/components/main/Products.jsx
+++ src/components/main/Products.jsx
@@ -65,7 +65,7 @@ function Products() {
                     <motion.button
                       className={clsx(
                         "cursor-pointer bg-secondary w-[50px] px-2 py-1 rounded-[3px] flex items-center justify-center   text-text  transition-all duration-600 hover:bg-secondary/80 active:bg-secondary/80 focus:bg-secondary/80",
-                        mobileHovered && "bg-secondary/80"
+                        mobileHovered && "bg-secondary/70"
                       )}
                       onTouchStart={() => setMobileHovered(true)}
                       onTouchEnd={() => setMobileHovered(false)}
