20a243f21efbc4566c16db5dc93a7fdbf17452da
Esther Okiki <estherokiki15@gmail.com>
2025-08-03 13:07:32 +0100

    fix: change products items cart hover effect

--- src/components/main/Products.jsx
+++ src/components/main/Products.jsx
@@ -64,7 +64,7 @@ function Products() {
                   {item.inStock ? (
                     <motion.button
                       className={clsx(
-                        "cursor-pointer bg-secondary w-[50px] px-2 py-1 rounded-[3px] flex items-center justify-center text-text border-2 border-secondary transition-all duration-600 hover:text-secondary hover:bg-transparent active:bg-transparent focus:bg-transparent active:text-secondary focus:text-secondary",
+                        "cursor-pointer bg-secondary w-[50px] px-2 py-1 rounded-[3px] flex items-center justify-center text-text border-2 border-secondary transition-all duration-600 hover:text-secondary hover:bg-transparent",
                         hoveredItemId === item.id
                           ? "bg-transparent text-secondary border-secondary"
                           : "bg-secondary text-text border-secondary"
