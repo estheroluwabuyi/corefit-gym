7337836613e542f23f83a3bf8640ea5b55eeb302
Esther Okiki <estherokiki15@gmail.com>
2025-10-27 12:18:45 +0100

    removed useMemo from values

--- src/contexts/GymContext.jsx
+++ src/contexts/GymContext.jsx
@@ -1,5 +1,4 @@
-import { createContext, useContext, useMemo, useState } from "react";
-
+import { createContext, useContext, useState } from "react";
 import { equipmentItems } from "../utils/equipmentList";
 import { contactInfo } from "../utils/contactPageInfo";
 
@@ -7,30 +6,30 @@ const GymContext = createContext();
 
 function GymProvider({ children }) {
   const [equipments, setEquipments] = useState(equipmentItems);
-  const contactPage = contactInfo;
   const [hoveredItemId, setHoveredItemId] = useState(null);
   const [containerActive, setContainerActive] = useState(null);
 
-  const value = useMemo(() => {
-    return {
-      equipments: equipments,
-      setEquipments: setEquipments,
-      hoveredItemId: hoveredItemId,
-      setHoveredItemId: setHoveredItemId,
-      containerActive: containerActive,
-      setContainerActive: setContainerActive,
-      contactPage: contactPage,
-    };
-  }, [contactPage, containerActive, equipments, hoveredItemId]);
-
-  return <GymContext.Provider value={value}>{children}</GymContext.Provider>;
+  return (
+    <GymContext.Provider
+      value={{
+        equipments,
+        setEquipments,
+        hoveredItemId,
+        setHoveredItemId,
+        containerActive,
+        setContainerActive,
+        contactPage: contactInfo,
+      }}
+    >
+      {children}
+    </GymContext.Provider>
+  );
 }
 
 function useGym() {
   const context = useContext(GymContext);
   if (context === undefined)
     throw new Error("useGym must be used within a GymProvider");
-
   return context;
 }
 
