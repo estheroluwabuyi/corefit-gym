2c3e0185eca8ad64ac0d5a0c1edc4443e920a870
Esther Okiki <estherokiki15@gmail.com>
2025-12-07 16:13:56 +0100

    clean card component

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -58,10 +58,6 @@ function CardComponent({ item, addToCart }) {
             onTouchEnd={() => setHoveredItemId(null)}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.9 }}
-            // onClick={(e) => {
-            //   e.preventDefault();
-            //   addToCart();
-            // }}
           >
             <FaShoppingCart />
           </motion.button>
